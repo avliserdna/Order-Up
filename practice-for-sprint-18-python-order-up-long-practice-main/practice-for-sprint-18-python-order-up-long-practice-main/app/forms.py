59a348a2cd57cf7859cfd1306988f877f014b7f7
Andre <andre.christian.silva@gmail.com>
2022-12-05 21:52:18 -0800

    Added forms.

---
+++ practice-for-sprint-18-python-order-up-long-practice-main/practice-for-sprint-18-python-order-up-long-practice-main/app/forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+class LoginForm(FlaskForm):
+    employee_number = StringField("Employee Number", DataRequired())
+    password = PasswordField("Password", DataRequired())
+    submit = SubmitField("Login")
